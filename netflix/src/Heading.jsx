150cadcb76b7ac64f65a9c9b7943e8667826988b
PRANAV TRIPATHI <pranavtri00@gmail.com>
2021-07-20 20:20:51 +0530

    Delete netflix directory

--- netflix/src/Heading.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Heading = (props) => {
-  return <h3 className="card_heading">{props.name}</h3>;
-};
-
-export default Heading;
--- netflix/src/Images.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Images = (props) => {
-  return <img src={props.imgsrc} alt="Image" className="card_image" />;
-};
-
-export default Images;
